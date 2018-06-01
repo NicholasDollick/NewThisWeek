f6a6326dceea66afe93155a1664208146414d35a
NicholasDollick <nidollick@gmail.com>
2018-06-01 02:32:16 -0700

    initial file setup

--- SpotifyInterface/Form1.Designer.cs
+++ SpotifyInterface/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.authButton = new System.Windows.Forms.Button();
+            this.playlistsListBox = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(790, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // authButton
+            // 
+            this.authButton.Location = new System.Drawing.Point(-1, 26);
+            this.authButton.Name = "authButton";
+            this.authButton.Size = new System.Drawing.Size(945, 67);
+            this.authButton.TabIndex = 1;
+            this.authButton.Text = "Authenticate Session";
+            this.authButton.UseVisualStyleBackColor = true;
+            this.authButton.Click += new System.EventHandler(this.authButton_Click);
+            // 
+            // playlistsListBox
+            // 
+            this.playlistsListBox.FormattingEnabled = true;
+            this.playlistsListBox.Location = new System.Drawing.Point(12, 99);
+            this.playlistsListBox.Name = "playlistsListBox";
+            this.playlistsListBox.Size = new System.Drawing.Size(772, 342);
+            this.playlistsListBox.TabIndex = 13;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(903, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(301, 342);
+            this.listBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1355, 479);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.playlistsListBox);
+            this.Controls.Add(this.authButton);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button authButton;
+        private System.Windows.Forms.ListBox playlistsListBox;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- SpotifyInterface/Form1.cs
+++ SpotifyInterface/Form1.cs
@@ -5,16 +5,96 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Net;
 using System.Threading.Tasks;
+using System.Net.Http;
+using SpotifyAPI;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
 using System.Windows.Forms;
 
 namespace SpotifyInterface
 {
     public partial class Form1 : Form
     {
+        private readonly ProxyConfig proxyConfig;
+        private SpotifyWebAPI spotify;
+
+        private PrivateProfile profile;
+
+
+
         public Form1()
         {
             InitializeComponent();
+            proxyConfig = new ProxyConfig();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<SimplePlaylist> test = GetPlaylists();
+
+            foreach (SimplePlaylist list in test)
+            {
+                playlistsListBox.Items.Add(list.Href);
+                listBox1.Items.Add(list.Name);
+            }
+        }
+
+        private async void InitialSetup()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(InitialSetup));
+                return;
+            }
+
+            authButton.Enabled = false;
+            profile = await spotify.GetPrivateProfileAsync();
+        }
+
+        private List<SimplePlaylist> GetPlaylists()
+        {
+            Paging<SimplePlaylist> playlists = spotify.GetUserPlaylists(profile.Id);
+            List<SimplePlaylist> list = playlists.Items.ToList();
+
+            while (playlists.Next != null)
+            {
+                playlists = spotify.GetUserPlaylists(profile.Id, 20, playlists.Offset + playlists.Limit);
+                list.AddRange(playlists.Items);
+            }
+
+            return list;
+        }
+
+        private async void RunAuthentication()
+        {
+            WebAPIFactory webApiFactory = new WebAPIFactory(
+                "http://localhost", 8000, "spotify client id",
+                Scope.UserReadPrivate | Scope.UserReadEmail | Scope.PlaylistReadPrivate | Scope.UserLibraryRead |
+                Scope.UserReadPrivate | Scope.UserFollowRead | Scope.UserReadBirthdate | Scope.UserTopRead | Scope.PlaylistReadCollaborative |
+                Scope.UserReadRecentlyPlayed | Scope.UserReadPlaybackState | Scope.UserModifyPlaybackState);
+
+            try
+            {
+                spotify = await webApiFactory.GetWebApi();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (spotify == null)
+                return;
+
+            InitialSetup();
+        }
+
+        private void authButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => RunAuthentication());
         }
     }
 }
